1184978879d31eed92ca2d03baf7198b5f0b1951
Agustin <fleitasagustinezequiel@gmail.com>
2026-05-18 23:55:38 -0300

    contar caracter y buscar caracter

--- funciones.py
+++ funciones.py
@@ -1,26 +1,26 @@
 
 
-def verif_clave(contrasena):
+def verif_clave(contra):
 
     # no puede estar vacía
-    if not contrasena:
+    if not contra:
         return False, "La contraseña no puede estar vacía."
 
     # no puede comenzar con espacios
-    if contrasena[0] == " ":
+    if contra[0] == " ":
         return False, "La contraseña no puede comenzar con espacios."
 
     # debe tener al menos 8 caracteres
-    if len(contrasena) < 8:
+    if len(contra) < 8:
         return False, "La contraseña debe tener al menos 8 caracteres."
 
     # debe contener al menos una letra
     tiene_letra = False
     i = 0
 
-    while i < len(contrasena):
+    while i < len(contra):
 
-        caracter = contrasena[i]
+        caracter = contra[i]
 
         if (caracter >= 'a' and caracter <= 'z') or (caracter >= 'A' and caracter <= 'Z'):
             tiene_letra = True
@@ -36,19 +36,19 @@ def verif_clave(contrasena):
 
 
 
-def val_seg(contrasena):
+def val_seg(contra):
 
     tiene_letras = False
     tiene_numeros = False
     tiene_simbolos = False
 
-    simbolos = '!\"#$%&\'()*+,-./'
+    simbolos = """!"#$%&'()*+,-./"""
 
     i = 0
 
-    while i < len(contrasena):
+    while i < len(contra):
 
-        caracter = contrasena[i]
+        caracter = contra[i]
 
         # verificar letras
         if (caracter >= 'a' and caracter <= 'z') or (caracter >= 'A' and caracter <= 'Z'):
@@ -73,7 +73,7 @@ def val_seg(contrasena):
         i = i + 1
 
     # CONTRASEÑA FUERTE
-    if len(contrasena) >= 12 and tiene_letras and tiene_numeros and tiene_simbolos:
+    if len(contra) >= 12 and tiene_letras and tiene_numeros and tiene_simbolos:
         return "Fuerte"
 
     # CONTRASEÑA MEDIA
@@ -81,10 +81,62 @@ def val_seg(contrasena):
         return "Media"
 
     # CONTRASEÑA DÉBIL
-    elif len(contrasena) >= 8 and len(contrasena) <= 9 and tiene_letras and not tiene_numeros and not tiene_simbolos:
+    elif len(contra) >= 8 and len(contra) <= 9 and tiene_letras and not tiene_numeros and not tiene_simbolos:
         return "Débil"
 
     
+
+
+def contador_caracteres(contra):
+
+    cantidad_letras = 0
+    cantidad_numeros = 0
+    cantidad_simbolos = 0
+    cantidad_espacios = 0
+
+    for caracter in contra:
+
+        #devuelve el codigo ascii de un caracter 
+        codigo = ord(caracter)
+
+        # Letras
+        if (codigo >= 65 and codigo <= 90) or (codigo >= 97 and codigo <= 122):
+            cantidad_letras += 1
+
+        # Números
+        elif codigo >= 48 and codigo <= 57:
+            cantidad_numeros += 1
+
+        # Espacios
+        elif codigo == 32:
+            cantidad_espacios += 1
+
+        # Símbolos
+        else:
+            cantidad_simbolos += 1
+
+    print("Cantidad de letras:", cantidad_letras)
+    print("Cantidad de números:", cantidad_numeros)
+    print("Cantidad de símbolos:", cantidad_simbolos)
+    print("Cantidad de espacios:", cantidad_espacios)
+
+
+
+
+def buscar_caracter(contra, caracter):
+
+    cantidad = 0
+
+    print("Posiciones donde aparece:")
+
+    for i in range(len(contra)):
+
+        if contra[i] == caracter:
+            cantidad += 1
+            print(i)
+
+    print("Cantidad de veces que aparece:", cantidad)
+
 
 
 
--- practica-examen.py
+++ practica-examen.py
@@ -9,14 +9,22 @@ while True :
             break
         
 print("\n1 validar nivel de seguridad\n2 contar tipos de caracteres\n3 buscar caracter especifico\n4 mostrar contraseña invertida")
-print("5 generar reporte estadistico\n6 verificar si es palidromo\n7 ordenar caracteres de la contraseña\n8 salir")
+print("5 generar reporte estadistico\n6 verificar si es palidromo\n7 ordenar caracteres de la contraseña\n8 salir\n")
 menu = int(input("seleccione el numero que corresponda :"))
 
 if menu == 1:
     from funciones import val_seg
-    clave = clave
     nivel = val_seg(clave)
     print("Nivel de seguridad:", nivel)
 
 if menu == 2 :
-     
+    from funciones import contador_caracteres
+    clave = clave
+    contador_caracteres(clave)
+
+if menu == 3 :
+    from funciones import buscar_caracter
+    clave = clave
+    caracter = input("Ingrese un caracter a buscar: ")
+
+    buscar_caracter(clave, caracter)
